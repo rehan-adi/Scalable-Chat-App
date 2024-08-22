d738d5d612e3d53ea9ca20e0c489c1690d869d75
Rehan <rehanalire52@gmail.com>
2024-08-22 16:00:34 +0530

    Refactor server to use Express with HTTP server setup and root route for JSON response

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,1 +1,18 @@
-console.log('Working');
+import http from "http";
+import express, { Request, Response } from "express";
+
+const init = () => {
+  const app = express();
+  const server = http.createServer(app);
+  const port = process.env.PORT || 4000;
+
+  app.get("/", (req: Request, res: Response) => {
+    res.json({ success: true, message: "OK" });
+  });
+
+  server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+init();
