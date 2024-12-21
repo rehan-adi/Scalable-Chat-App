b336ecf3c70c83dba73fe85bcf05deb4af708295
Rehan <rehanalire52@gmail.com>
2024-12-21 22:30:19 +0530

    Remove username from Redis Publisher and Subscriber

--- apps/server/src/utils/redis.ts
+++ apps/server/src/utils/redis.ts
@@ -1,17 +1,15 @@
 import { Redis } from "ioredis";
 
 export const Publisher = new Redis({
-  host: process.env.HOST,
-  username: process.env.USERNAME,
-  port: Number(process.env.PORT),
-  password: process.env.PASSWORD,
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT) || 6379,
+  password: process.env.REDIS_PASSWORD,
 });
 
 export const Subscriber = new Redis({
-  host: process.env.HOST,
-  username: process.env.USERNAME,
-  port: Number(process.env.PORT),
-  password: process.env.PASSWORD,
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT) || 6379,
+  password: process.env.REDIS_PASSWORD,
 });
 
 Publisher.on("error", (err) => {
