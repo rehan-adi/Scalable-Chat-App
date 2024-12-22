ad61eacabf0c7fad7957a63215fd2bc7882ec012
Rehan <rehanalire52@gmail.com>
2024-12-22 20:04:22 +0530

    fix: update Redis configuration in .env.example and remove password from Redis client initialization

--- apps/server/src/utils/redis.ts
+++ apps/server/src/utils/redis.ts
@@ -3,13 +3,11 @@ import { Redis } from "ioredis";
 export const Publisher = new Redis({
   host: process.env.REDIS_HOST,
   port: Number(process.env.REDIS_PORT) || 6379,
-  password: process.env.REDIS_PASSWORD,
 });
 
 export const Subscriber = new Redis({
   host: process.env.REDIS_HOST,
   port: Number(process.env.REDIS_PORT) || 6379,
-  password: process.env.REDIS_PASSWORD,
 });
 
 Publisher.on("error", (err) => {
